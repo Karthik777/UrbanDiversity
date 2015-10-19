50b955076363cf96decf80e663f21ce99e8647c2
Karthik Rajgopal <karthik.cool7777@gmail.com>
2015-10-20 06:46:15 +1100

    mobile changes

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'Spot ME',
+  description: 'An app to record Urban Diversity',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': '/Icons/Insect-24.png',
+  'android_mdpi': 'resources/icons/Insect-32.png',
+  'android_hdpi': 'resources/icons/Insect-64.png',
+  'android_xhdpi': 'resources/icons/Insect-100.png'
+});
